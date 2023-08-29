b0d53d959dc4fb0b624ec6d8ce61e6b9ca199ba0
Aniket Narsikar <123082659+aniket-narsikar@users.noreply.github.com>
2023-08-29 15:15:30 +0530

    Update UserDto.java

--- src/main/java/com/edu/aniket/dto/UserDto.java
+++ src/main/java/com/edu/aniket/dto/UserDto.java
@@ -1,5 +1,20 @@
 package com.edu.aniket.dto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
+	private long id;
+	private String name;
+	private String email;
+	private Role role;
+	
 
 }
