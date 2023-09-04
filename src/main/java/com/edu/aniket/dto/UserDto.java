a36f881eb373cabe73ca6a0116fc20507e47dff9
Aniket <narsikaraniket@gmail.com>
2023-09-04 20:55:12 +0530

    4th sept 1st commit

--- src/main/java/com/edu/aniket/dto/UserDto.java
+++ src/main/java/com/edu/aniket/dto/UserDto.java
@@ -1,5 +1,5 @@
 package com.edu.aniket.dto;
-
+import com.edu.aniket.entity.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,4 +16,5 @@ public class UserDto {
 	private String name;
 	private String email;
 	// The 'Role' import has been removed, so you can remove 'private Role role;' as well
+	private Role role;
 }
